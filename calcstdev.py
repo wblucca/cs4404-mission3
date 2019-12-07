fe88bd26700874d6b72127e5c9046b8bb7040b4a
wblucca <wblucca@wpi.edu>
2019-12-07 14:09:04 -0500

    stddev calculator done, needs testing

--- calcstdev.py
+++ calcstdev.py
@@ -20,15 +20,19 @@ if __name__ == "__main__":
     # Initialize charscount dictionary
     for c in domainchars:
         charscount[c] = 0
+    
+    print('Using chars: ' + domainchars)
 
     if not getcsv():
         print('Failed to get Alexa 1 million list')
         exit(9)
 
+    # Total characters counted
+    totalchars = 0
+
     # Open the CSV
     with open('alexa-1m.csv') as csvfile:
         # Count occurrences of each char in domainchars in Alexa 1m list
-        totalchars = 0
         for row in csvfile:
             # Get domain name minus all characters after first '.'
             domain = re.sub(r'\..*', '', row[1])
@@ -39,8 +43,14 @@ if __name__ == "__main__":
                 else:
                     print(c)
                     
-        # Calculate average occurence of each char
-        average = totalchars / len(domainchars)
+    # Calculate average occurence of each char
+    average = totalchars / len(domainchars)
 
-        # Calculate std deviation
-        #for c in domainchars:
+    # Calculate std deviation
+    totalsqrdiff = 0
+    for c in domainchars:
+        totalsqrdiff += (charscount[c] - average) ** 2
+    stddev = (totalsqrdiff / (len(domainchars) - 1)) ** 0.5
+
+    print('Standard deviation for top 1 million sites: ' + str(stddev))
+    return stddev
