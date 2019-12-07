1b331ba68ef7f86f6fb7afcd5f887ffbb9dbc082
wblucca <wblucca@wpi.edu>
2019-12-07 15:32:04 -0500

    Calculations working

--- calcstdev.py
+++ calcstdev.py
@@ -1,27 +1,25 @@
 #! /usr/bin/env python2.7
 
-import os, csv
+import os, re, csv, random
 
 domainchars = 'abcdefghijklmnopqrstuvwxyz0123456789'
 charscount = {}
 
 def getcsv():
     # Get CSV zip file
-    if os.system('curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip --output alexa-1m.csv.zip') != 0:
-        return false
+    if os.system('curl http://s3.amazonaws.com/alexa-static/top-1m.csv.zip --output alexa-1m.csv.zip > /dev/null') != 0:
+        return False
     # Unzip to alexa-1m.csv
-    if os.system('unzip alexa-1m.csv.zip') != 0:
-        return false
+    if os.system('unzip -f alexa-1m.csv.zip > /dev/null') != 0:
+        return False
 
     # No errors
-    return true
+    return True
 
 if __name__ == "__main__":
     # Initialize charscount dictionary
     for c in domainchars:
         charscount[c] = 0
-    
-    print('Using chars: ' + domainchars)
 
     if not getcsv():
         print('Failed to get Alexa 1 million list')
@@ -31,26 +29,32 @@ if __name__ == "__main__":
     totalchars = 0
 
     # Open the CSV
-    with open('alexa-1m.csv') as csvfile:
+    with open('top-1m.csv') as csvfile:
         # Count occurrences of each char in domainchars in Alexa 1m list
-        for row in csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
             # Get domain name minus all characters after first '.'
             domain = re.sub(r'\..*', '', row[1])
+            #domain = random.choice(domainchars)
             for c in domain:
                 if c in domainchars:
                     totalchars += 1
                     charscount[c] += 1
-                else:
-                    print(c)
                     
     # Calculate average occurence of each char
     average = totalchars / len(domainchars)
-
-    # Calculate std deviation
+    
+    # Calculate std deviation and coefficient of variation
     totalsqrdiff = 0
     for c in domainchars:
         totalsqrdiff += (charscount[c] - average) ** 2
+        print(c + ': ' + str(charscount[c]))
     stddev = (totalsqrdiff / (len(domainchars) - 1)) ** 0.5
+    cv = stddev / average
 
-    print('Standard deviation for top 1 million sites: ' + str(stddev))
-    return stddev
+    # Print info from data
+    print('Using chars: ' + domainchars)
+    print('Characters counted: ' + str(totalchars))
+    print('Average character count: ' + str(average))
+    print('Stdev:\t' + str(stddev))
+    print('CV:\t' + str(cv))
